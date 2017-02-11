bdd67ab254a2104b0127608f202fef2b3df9a037
Xero <x3rosnow@gmail.com>
2017-02-11 21:03:24 +0530

    Adding testng instead of junit

--- firstProject/src/test/java/steps/ClientSteps.java
+++ firstProject/src/test/java/steps/ClientSteps.java
@@ -3,11 +3,12 @@ package steps;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 
+import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
-import junit.framework.Assert;
 
 public class ClientSteps {
 
@@ -27,27 +28,27 @@ public class ClientSteps {
 		driver.findElement(By.linkText("REGISTER")).click();
 	}
 
-	@When("^User fills in the Contact Info$")
+	@And("^User fills in the Contact Info$")
 	public void fillContactInfo() throws Throwable {
 		driver.findElement(By.name("firstName")).sendKeys("John");
 	}
 
-	@When("^User enters User information$")
+	@And("^User enters User information$")
 	public void fillUserInfo() throws Throwable {
 		driver.findElement(By.id("email")).sendKeys("John");
 		driver.findElement(By.name("password")).sendKeys("John");
 		driver.findElement(By.name("confirmPassword")).sendKeys("John");
 	}
 
-	@When("^User selects Submit$")
+	@And("^User selects Submit$")
 	public void selectSubmit() throws Throwable {
 		driver.findElement(By.name("register")).click();
 	}
 
 	@Then("^System should show the thank you page$")
 	public void verifyConfirmation() throws Throwable {
-		Assert.assertTrue("No Match Found",
-				driver.getCurrentUrl().equals("http://newtours.demoaut.com/create_account_success.php"));
+		Assert.assertTrue(driver.getCurrentUrl().equals("http://newtours.demoaut.com/create_account_success.php"),
+				"No Match Found");
 		driver.close();
 	}
 }
